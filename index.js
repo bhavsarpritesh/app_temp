fd04d1ad35ffbfeab6963331616bfeca339619c0
bhavsarpritesh <priteshbhav9268@gmail.com>
2022-07-21 13:32:22 +0530

    added  index.js-secondTime

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Welcome to my git session");
+console.log("Welcome to my channal");
+console.log("Welcome to saama");
+console.log("Welcome to saama insurnace anlaytics");
+console.log("thank you ");
